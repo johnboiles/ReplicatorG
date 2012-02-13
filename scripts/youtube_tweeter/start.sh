a4b118ab39ae09e452687b5e42e29c2cebc26d8c
John Boiles <johnaboiles@gmail.com>
2012-02-13 14:02:34 -0800

    Fixing start script

--- scripts/youtube_tweeter/start.sh
+++ scripts/youtube_tweeter/start.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+SCRIPT_DIR=$(cd `dirname $0` && pwd)
+source "$SCRIPT_DIR"/config.sh
+
 FILE_PATH=$1
 FILE_NAME=${FILE_PATH##*/}
 ORIGINAL_VIDEO=$FILE_NAME.m4v
 
 sleep 90
-osascript scripts/start_recording.scpt "$ORIGINAL_VIDEO"
+osascript "$SCRIPT_DIR"/start_recording.scpt "$ORIGINAL_VIDEO"
